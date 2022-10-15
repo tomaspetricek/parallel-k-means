705735581eb132d6a44ab0e700ddcc21dbaf805f
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2022-10-15 19:33:23 +0200

    Measure duration

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <random>
 #include <filesystem>
+#include <chrono>
+#include <locale>
 #include <array.hpp>
 #include <k_means.hpp>
-
 
 template<std::size_t n_samples, std::size_t n_features, std::size_t n_clusters>
 void initialize_centroids(const double (& samples)[n_samples][n_features],
@@ -19,6 +20,11 @@ void initialize_centroids(const double (& samples)[n_samples][n_features],
     }
 }
 
+template<typename CharType>
+struct num_separator : public std::numpunct<CharType> {
+    std::string do_grouping() const override { return "\003"; }
+    CharType do_thousands_sep() const override { return ' '; }
+};
 
 int main()
 {
@@ -31,18 +37,28 @@ int main()
 
     // read
     char delim{','};
-    std::filesystem::path data_dir{"src/data"};
+    std::filesystem::path data_dir{"data"};
     mcc::read_csv(data_dir/"samples.csv", delim, samples);
     mcc::read_csv(data_dir/"labels.csv", delim, actual_labels);
     mcc::read_csv(data_dir/"init_centroids.csv", delim, centroids);
 
     // cluster
     mcc::clustering::k_means<n_samples, n_features, n_clusters> cluster;
+    auto begin = std::chrono::high_resolution_clock::now();
     cluster(samples, labels, centroids);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin);
 
     // log results
-    std::cout << "n iterations: " << cluster.n_iter() << std::endl;
-    std::cout << "centroids: " << std::endl;
+    // use custom number separator
+    std::cout.imbue(std::locale(std::cout.getloc(), new num_separator<char>()));
+
+    std::cout << "n samples: " << n_samples << std::endl
+              << "n features: " << n_features << std::endl
+              << "n clusters: " << n_clusters << std::endl
+              << "n iterations: " << cluster.n_iter() << std::endl
+              << "duration: " << duration.count() << " ns" << std::endl
+              << "centroids: " << std::endl;
     mcc::print(centroids);
     std::cout << "labels: " << std::endl;
     mcc::print(labels);
