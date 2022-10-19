cef375cf23b5b7d9c5f9a47250b9d2b33d8052e9
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2022-10-19 13:36:43 +0200

    Add same size samples

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import make_blobs
 import numpy as np
+import os
 
 
 def compute_distances(samples, centroids):
@@ -21,29 +22,43 @@ def compute_centroids(samples, labels, n_clusters):
     return centroids
 
 
-def generate_samples(n_samples, n_features, centers):
-    samples, labels, centers = make_blobs(n_samples=n_samples,
-                                          n_features=n_features,
-                                          centers=centers,
-                                          cluster_std=0.8,
-                                          shuffle=True,
-                                          return_centers=True)
+def generate_same_size_samples(min_exp, max_exp, n_centers, min_n_samples):
+    assert (min_exp < max_exp)
+    n_nums = (2 ** max_exp) * min_n_samples
+    res = list()
 
-    np.savetxt("data/labels.csv", labels, delimiter=",")
-    np.savetxt("data/samples.csv", samples, delimiter=",")
-    np.savetxt("data/centers.csv", centers, delimiter=",")
+    for exp in range(min_exp, max_exp + 1, 1):
+        n_features = (2 ** exp)
+        n_samples = int(n_nums / n_features)
+        samples, labels, centers = make_blobs(n_samples=n_samples,
+                                              n_features=n_features,
+                                              centers=n_centers,
+                                              cluster_std=0.8,
+                                              shuffle=True,
+                                              return_centers=True)
+        res.append((n_samples, n_features, n_centers, samples, labels, centers))
+
+    return res
+
+
+def save_data(data, dir_path, data_dir_fmt):
+    for n_samples, n_features, n_centers, samples, labels, centers in data:
+        # create data directory
+        data_dir_name = data_dir_fmt.format(n_samples=n_samples, n_features=n_features, n_centers=n_centers)
+        data_dir_path = os.path.join(dir_path, data_dir_name)
+        os.mkdir(data_dir_path)
+
+        # save data
+        np.savetxt(os.path.join(data_dir_path, "labels.csv"), labels, delimiter=",")
+        np.savetxt(os.path.join(data_dir_path, "samples.csv"), samples, delimiter=",")
+        np.savetxt(os.path.join(data_dir_path, "centers.csv"), centers, delimiter=",")
 
 
 def main():
-    samples = np.genfromtxt("data/test/samples.csv", delimiter=',')
-    labels = np.genfromtxt("data/test/labels.csv", delimiter=',')
-    centers = np.genfromtxt("data/test/centers.csv", delimiter=',')
-
-    centroids = compute_centroids(samples, labels, centers.shape[0])
-    np.savetxt("data/test/centroids.csv", centroids, delimiter=',')
-
-    dists = compute_distances(samples, centroids)
-    np.savetxt("data/test/distances.csv", dists, delimiter=',')
+    data = generate_same_size_samples(1, 8, 10, 5_000)
+    src_data_dir_path = "data"
+    data_dir_fmt = "{n_samples}-{n_features}-{n_centers}"
+    save_data(data, src_data_dir_path, data_dir_fmt)
 
 
 if __name__ == "__main__":
